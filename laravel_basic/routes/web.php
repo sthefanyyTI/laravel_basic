e7ae672e44d4aa8e0714d5d2287859d66419127c
Sthefany <sthefanyy.TI@gmail.com>
2026-01-22 13:43:17 -0300

    Edição de salas

--- laravel_basic/routes/web.php
+++ laravel_basic/routes/web.php
@@ -9,13 +9,11 @@ Route::get('/salas/create', [SalaController::class, 'create'])->middleware('auth
 
 Route::get('/salas/{id}', [SalaController::class, 'show']);
 
-//Route::get('/salas/{id}/edit', [SalaController::class, 'edit']);
+Route::get('/salas/edit/{id}', [SalaController::class, 'edit'])->middleware('auth');
 
 Route::post('/salas', [SalaController::class, 'store']);
 
 Route::delete('/salas/{id}', [SalaController::class, 'destroy'])->middleware('auth');
-
-Route::get('/salas/edit/{id}', [SalaController::class, 'edit'])->middleware('auth');
 
 //Route::put('/salas/{id}', [SalaController::class, 'update'])->middleware('auth');
 
